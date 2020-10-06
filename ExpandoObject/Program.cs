1c0de1aba04e67d18dff86b61de53f1acf42e673
Tim Mahy <timm@infosupport.com>
2020-10-06 11:02:09 +0200

    dynamic demos

--- ExpandoObject/Program.cs
+++ ExpandoObject/Program.cs
@@ -15,23 +15,53 @@ namespace ExpandoObject
             d.Z = "10";
             WriteLine(d.Z);
             WriteLine(d.Z.GetType());
+    
             // d.Z = 100;
             // WriteLine(d.Z);
             // WriteLine(d.Z.GetType());
-            //
+            
             // d.Print = new Action<string>((s) => WriteLine(s));
             // d.Print("test");
-            //
+            
             // d.Event = null;
             // d.Event += new Action<object,EventArgs>((o, eventArgs) => WriteLine(o) );
             // d.Event("testEvent", new EventArgs());
-            //
+            
+            
+            // foreach (var z in d)
+            // {
+            //     Console.WriteLine(z.Key);   
+            // }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             // IDictionary<string, object> z = d;
             // foreach (var i in z)
             // {
             //     WriteLine(i);
             // }
             //
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             // z.Remove("Print");
             // d = z;
             // foreach (var i in z)
@@ -39,6 +69,20 @@ namespace ExpandoObject
             //     WriteLine(i);
             // }
             //
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             // ((INotifyPropertyChanged) d).PropertyChanged += (sender, eventArgs) => WriteLine(eventArgs.PropertyName);
             // d.Z = "500";
             // d.Z = "1500";
